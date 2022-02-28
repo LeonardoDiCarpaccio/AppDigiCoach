7f177fca2e7154001e4f52f251c8470a801d0a09
LeonardoDiCarpaccio <59809554+LeonardoDiCarpaccio@users.noreply.github.com>
2022-02-28 18:39:12 +0100

    skills and update home notif and skills recap

--- src/app/views/player/skill/skill.component.ts
+++ src/app/views/player/skill/skill.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { PreviewPlayerService } from 'src/app/services/preview-player.service';
 
 @Component({
   selector: 'app-skill',
@@ -9,13 +11,24 @@ export class SkillComponent implements OnInit {
   arr = [1,1,1,1]
   arrTitle = ["Physique","Technique","Tactique","Mental"]
 
+  skillsList : any
+  arraySkillsPhy : any
+  arraySkillsTac : any
+  arraySkillsTec : any
+  arraySkillsMen : any
+
   currentIndex = 0
-  constructor() { }
+  
+  constructor(private previewPlayer : PreviewPlayerService,private auth: AuthService) { }
   slideOpts = {
     initialSlide: 0,
     speed: 400
   };
-  ngOnInit() {}
+  ngOnInit() {
+  this.getSkillsListPlayer()
+  
+    console.log(  this.skillsList,"  this.skillsList")
+  }
 
   goLeftSlide(){
     if(this.currentIndex != 0){
@@ -30,5 +43,23 @@ export class SkillComponent implements OnInit {
       console.log("this.currentIndex",this.currentIndex)
 
     }
+  }
+
+  async getSkillsListPlayer(){
+    await this.previewPlayer.getSkillByPlayer({id_player : this.auth.getId()}).subscribe((res)=>{
+      console.log(res,"res")
+      this.arraySkillsPhy = res["physique"] != undefined ?res["physique"]  : null
+this.arraySkillsTac = res["tactique"] != undefined ? res["tactique"] : null
+this.arraySkillsTec = res["technique"] != undefined ? res["technique"] : null
+this.arraySkillsMen = res["mental"] != undefined ? res["mental"]  : null
+console.log(this.arraySkillsTac,"arraySkillsTac")
+
+console.log(this.arraySkillsTec,"arraySkillsTec")
+
+console.log(this.arraySkillsMen,"this.arraySkillsMen")
+
+console.log( this.arraySkillsPhy," this.arraySkillsPhy")
+
+     })
   }
 }
